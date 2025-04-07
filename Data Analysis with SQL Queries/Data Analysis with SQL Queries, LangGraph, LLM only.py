6b8a10caa4b9c40be62cb7abb61aee0229ffed28
Tsvetina <114422946+Tsvetina17@users.noreply.github.com>
2025-04-07 15:46:41 +0200

    new prompt

--- Data Analysis with SQL Queries/Data Analysis with SQL Queries, LangGraph, LLM only.py
+++ Data Analysis with SQL Queries/Data Analysis with SQL Queries, LangGraph, LLM only.py
@@ -14,6 +14,7 @@ import os
 import yaml
 from dotenv import load_dotenv
 from pathlib import Path
+import glob
 
 import json
 import logging
--- Data Analysis with SQL Queries/Data Analysis with SQL Queries, LangGraph, Simple Agent.py
+++ Data Analysis with SQL Queries/Data Analysis with SQL Queries, LangGraph, Simple Agent.py
@@ -165,14 +165,18 @@ query_gen_system = """You are a SQL expert with a strong attention to detail.
 
 Given an input question, output a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
 
-DO NOT call any tool besides SubmitFinalAnswer to submit the final answer.
+DO NOT call any tool besides the tools that you were given.
 
 When generating the query:
 
-Output the SQL query that answers the input question without a tool call.
+Use the tools list_tables_tool and get_schema_tool to find the correct table names and column names from the database schema. 
 
-Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
-You can order the results by a relevant column to return the most interesting examples in the database.
+Then use the information to write an SQL query that answers the input question.
+
+Execute the SQL query using the execute_sql_tool tool. 
+
+Based on the results of the query, return the answer to the input question as a simple numerical value.
+
 Never query for all the columns from a specific table, only ask for the relevant columns given the question.
 
 If you get an error while executing a query, rewrite the query and try again.
@@ -180,7 +184,7 @@ If you get an error while executing a query, rewrite the query and try again.
 If you get an empty result set, you should try to rewrite the query to get a non-empty result set. 
 NEVER make stuff up if you don't have enough information to answer the query... just say you don't have enough information.
 
-If you have enough information to answer the input question, simply invoke the appropriate tool to submit the final answer to the user.
+If you have enough information to answer the input question, simply output the final answer to the user.
 
 DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database."""
 query_gen_prompt = ChatPromptTemplate.from_messages(
